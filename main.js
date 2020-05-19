d2587154c629c403b267a3162bf84528e990e65c
Roberth <rgonzalez.daw18@lamerce.academy>
2020-05-20 01:39:20 +0200

    Toggle vertical scroll

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
-  document.querySelector(".nav-menu").classList.toggle("show");
+  const navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.toggle("show");
+  // Toggle vertical scroll
+  navMenu.addEventListener("transitionend", () => {
+    const overflow = navMenu.classList.contains("show") ? "hidden" : "scroll";
+    document.body.style.overflowY = overflow;
+  });
 });
 
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
-
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".news-cards", { delay: 500 });
+ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
